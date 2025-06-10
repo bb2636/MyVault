f03b824281af7f42102310c59ec56790166d19b8
[SW7] 이수빈 <dltnqls3267@naver.com>
2025-06-10 17:40:54 +0900

    Feat: UserService추가

--- src/main/java/org/example/myvault/service/UserService.java
+++ src/main/java/org/example/myvault/service/UserService.java
@@ -1,4 +1,20 @@
 package org.example.myvault.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.myvault.domain.User;
+import org.example.myvault.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+    public User save(User user) {
+        return userRepository.save(user);
+    }
+    public Optional<User> findById(Long id) {
+        return userRepository.findById(id);
+    }
 }

013020c60c958372051bf49e2b57cba5ad8a4a2c
[SW7] 이수빈 <dltnqls3267@naver.com>
2025-06-10 17:43:22 +0900

    Feat: CollectionService추가

--- src/main/java/org/example/myvault/service/CollectionItemService.java
+++ src/main/java/org/example/myvault/service/CollectionItemService.java
@@ -1,4 +1,27 @@
 package org.example.myvault.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.myvault.domain.CollectionItem;
+import org.example.myvault.repository.CollectionItemRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
 public class CollectionItemService {
+    private final CollectionItemRepository collectionItemRepository;
+
+    public CollectionItem save(CollectionItem item) {
+        return collectionItemRepository.save(item);
+    }
+
+    public List<CollectionItem> findAll() {
+        return collectionItemRepository.findAll();
+    }
+
+    public Optional<CollectionItem> findById(Long id) {
+        return collectionItemRepository.findById(id);
+    }
 }

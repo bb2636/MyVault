bea077798b6c6f515de1a496e1ab515c3568eef3
[SW7] 이수빈 <dltnqls3267@naver.com>
2025-06-10 17:45:45 +0900

    Feat: CommentService추가

--- src/main/java/org/example/myvault/service/CommentService.java
+++ src/main/java/org/example/myvault/service/CommentService.java
@@ -1,4 +1,23 @@
 package org.example.myvault.service;
 
+import lombok.RequiredArgsConstructor;
+import org.example.myvault.domain.CollectionItem;
+import org.example.myvault.domain.Comment;
+import org.example.myvault.repository.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class CommentService {
+    private final CommentRepository commentRepository;
+
+    public Comment save(Comment comment) {
+        return commentRepository.save(comment);
+    }
+
+    public List<Comment> findByCollectionItem(CollectionItem item) {
+        return commentRepository.findByCollectionItem(item);
+    }
 }
